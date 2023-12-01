761e0063e45558f7d0f4a9e74e7b6de64440616c
MilenSemkov <151528825+MilenSemkov@users.noreply.github.com>
2023-12-02 00:41:52 +0200

    Add files via upload

---
+++ DvanaistaZadacha/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DvanaistaZadacha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Put a word: ");
+            string duma=Console.ReadLine();
+            List<char> list = duma.ToList();
+            list.Reverse();
+            
+            Console.WriteLine("The reverse word is "+string.Join("",list).ToLower());
+        }
+    }
+}
